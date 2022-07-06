e484b5b9f293b0a3f8352fdcaffd0a615285cfc8
JiYoung <yj0zero@gmail.com>
2022-07-06 22:45:01 +0900

    Create App.js

---
+++ components/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      Subject:{title: "WEB", sub: "world wide web!"},
+      contents:[
+        {id:1, title: "HTML", desc: "HTML is for information"},
+        {id:2, title: "CSS", desc: "CSS is for design"},
+        {id:3, title: "JavaScript", desc: "JavaScript is for interactive"}
+
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <Subject title={this.state.Subject.title} sub={this.state.Subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
+}
+
+export default App;
